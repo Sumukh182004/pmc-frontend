82f4832d4981ed597cef6f3597bb3f1b8cab2520
Sumukh <sumukhsoni119@gmail.com>
2026-04-02 18:09:18 +0530

    Initial commit - PMC Frontend (React)

---
+++ src/Admin/SignedUrl.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+let bucketName = 'pmctender';
+const triggerFunction = async (extension,foldername) => {
+
+  let earr = extension.split('.');
+  
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure month is two digits
+  
+  // let key1 = '';
+  // if (foldername[foldername.length - 1] === '/') {
+  //   key1 = `${year}/${month}/${foldername}${earr[0]}${Date.now()}.${earr[earr.length - 1]}`;
+  // } else if (foldername.length !== 0) {
+  //   key1 = `${year}/${month}/${foldername}/${earr[0]}${Date.now()}.${earr[earr.length - 1]}`;
+  // } else {
+  //   key1 = `${year}/${month}/${earr[0]}${Date.now()}.${earr[earr.length - 1]}`;
+  // }
+  
+
+
+  // let key1=${foldername}/${Date.now().toString()}.${extension}
+let key1=``
+// let key1 = foldername ? `${foldername}/${extension}` : `/${extension}`;
+console.log(foldername,foldername[foldername.length-1])
+if(foldername[foldername.length-1]=='/'){
+  // console.log("here")
+  key1=`${foldername}${extension}`
+}
+else if(foldername.length!=0){
+  console.log("there")
+  key1 = `${foldername}/${extension}` 
+}
+else{
+  key1=`/${extension}`
+}
+try {
+  const response = await axios.post("http://localhost:4000/gsu", {
+    bucket: bucketName,
+    key1,
+    Expires: 300,
+    account: 'BZTECHSERVER',
+  });
+
+  let arr = [response.data.signedUrl, key1];
+  console.log(arr); // Log the array containing signedUrl and key1
+
+  return arr; // Return the array as the result of triggerFunction
+} catch (error) {
+  console.error('Error fetching signedUrl:', error);
+  throw error; // Propagate the error if needed
+}
+
+  }
+
+  const getPredefinedUrl = (key1) => {
+    const modi = encodeURIComponent(key1).replace(/\+/g, "%20");
+    return `https://${bucketName}.s3.ap-south-1.amazonaws.com/${modi}`;
+};
+  
+  export { triggerFunction, getPredefinedUrl };
+
+
+  // right
+  //https://dcpr1.s3.ap-south-1.amazonaws.com/Appointment+of+developer+for+residential+redevelopment+of+Ambedkar+Nagar+CHS%2C+Worli/0%2B1+2()dfd.png
+  //rong
+  // https://dcpr1.s3.ap-south-1.amazonaws.com/Appointment+of+developer+for+residential+redevelopment+of+Ambedkar+Nagar+CHS,+Worli/0+1+2()dfd.png
---
+++ src/api.js
@@ -0,0 +1,10 @@
+// Dev
+// const baseurl="https://rmcqpi9a6k.execute-api.ap-south-1.amazonaws.com/prod"
+// Actual
+// const baseurl = "https://2xfu6r88mg.execute-api.ap-south-1.amazonaws.com/prod"
+
+// const baseurl="https://dv77uchjy2.execute-api.ap-south-1.amazonaws.com/prod"
+
+const baseurl="http://localhost:8000"
+
+export { baseurl }
